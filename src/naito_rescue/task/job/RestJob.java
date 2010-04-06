fba457494ac70557404babca5a225d2bd3aed49b
aitrescueboss <aitrescueboss@f95ec68d-f97c-3edf-3c56-82c48907e3e1>
2010-04-06 11:58:59 +0000

    各種ジョブの概形を取り揃えました

---
+++ src/naito_rescue/task/job/RestJob.java
@@ -0,0 +1,21 @@
+package naito_rescue.task.job;
+
+import rescuecore2.standard.components.*;
+import rescuecore2.standard.entities.*;
+import rescuecore2.worldmodel.*;
+import naito_rescue.task.*;
+import naito_rescue.agent.*;
+
+import java.util.*;
+import java.io.*;
+
+public class RestJob extends Job
+{
+	@Override
+	public void doJob(){
+	}
+
+	@Override
+	protected boolean isFinished(NAITOHumanoidAgent owner, StandardWorldModel model){
+	}
+}
