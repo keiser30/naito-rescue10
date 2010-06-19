05254b01e71c916d5b5c770443056480c38ae805
aitrescueboss <aitrescueboss@f95ec68d-f97c-3edf-3c56-82c48907e3e1>
2010-06-19 16:04:58 +0000

    Win用環境整備はじまり

---
+++ trunk/makeClassPath.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class makeClassPath
+{
+	public static void main(String[] args){
+		
+		if(args.length <= 0){
+			System.err.println("Usage: java makeClassPath <jar_dir>");
+			System.exit(-1);
+		}
+		StringBuffer classpath = new StringBuffer();
+		File jarDir = new File(args[0]);
+		File[] jarFiles = jarDir.listFiles();
+		
+		for(int i = 0;i < jarFiles.length-1;i++){
+			System.err.println(jarFiles[i].getName());
+			if(jarFiles[i].getPath().endsWith(".jar"))
+				classpath.append(jarFiles[i].getPath()).append(";");
+		}
+		classpath.append(jarFiles[jarFiles.length-1].getPath());
+		System.out.println("--------------------");
+		System.out.println(classpath.toString());
+	}
+}
